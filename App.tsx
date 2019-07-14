b02378bed647ab093fcf520456becfb790d62439
amanshharma <amandeep.upes@gmail.com>
2019-07-14 17:38:02 -0400

    Added list section and css styles

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
+type Props = {};
 interface IToDo {
   text: string;
   completed: boolean;
@@ -12,18 +13,86 @@ export default function App() {
   const [error, showError] = useState<Boolean>(false);
 
   return (
-    <View>
-      <Text>Todo List</Text>
-      <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Todo List</Text>
+      <View style={styles.inputWrapper}>
         <TextInput
           placeholder="Enter your todo task..."
-          value=""
+          value={value}
           onChangeText={e => {}}
+          style={styles.inputBox}
         />
         <Button title="Add Task" onPress={() => {}} />
       </View>
-      {error && <Text>Error: Input field is empty...</Text>}
-      <Text>Your Tasks :</Text>
+      {error && (
+        <Text style={styles.error}>Error: Input field is empty...</Text>
+      )}
+      <Text style={styles.subtitle}>Your Tasks :</Text>
+      {toDoList.length === 0 && <Text>No to do task available</Text>}
+      {toDoList.map((toDo: IToDo, index: number) => (
+        <View style={styles.listItem} key={`${index}_${toDo.text}`}>
+          <Text
+            style={[
+              styles.task,
+              { textDecorationLine: toDo.completed ? "line-through" : "none" }
+            ]}
+          >
+            {toDo.text}
+          </Text>
+          <Button
+            title={toDo.completed ? "Completed" : "Complete"}
+            onPress={() => {}}
+          />
+          <Button title="X" onPress={() => {}} color="crimson" />
+        </View>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 35,
+    alignItems: "center"
+  },
+  inputWrapper: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20
+  },
+  inputBox: {
+    width: 200,
+    borderColor: "purple",
+    borderRadius: 8,
+    borderWidth: 2,
+    paddingLeft: 8
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 40,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  },
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: "purple"
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 10
+  },
+  addButton: {
+    alignItems: "flex-end"
+  },
+  task: {
+    width: 200
+  },
+  error: {
+    color: "red"
+  }
+});
