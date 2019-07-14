8fcfd46ff1875603056165ec17a1d48dc595fed5
amanshharma <amandeep.upes@gmail.com>
2019-07-14 17:28:29 -0400

    Added basic layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+interface IToDo {
+  text: string;
+  completed: boolean;
+}
 
 export default function App() {
+  const [value, setValue] = useState<string>("");
+  const [toDoList, setToDos] = useState<IToDo[]>([]);
+  const [error, showError] = useState<Boolean>(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text>Todo List</Text>
+      <View>
+        <TextInput
+          placeholder="Enter your todo task..."
+          value=""
+          onChangeText={e => {}}
+        />
+        <Button title="Add Task" onPress={() => {}} />
+      </View>
+      {error && <Text>Error: Input field is empty...</Text>}
+      <Text>Your Tasks :</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
